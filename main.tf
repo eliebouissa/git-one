aa2c63f5a925c433156b3b3d8c07a534667b0feb
Elie Bou-Issa <elie.bouissa@microsoft.com>
2020-10-26 06:43:00 -0500

    Revert "renamed public ip"
    This reverts commit 5af3dc1e642a39916ba71721483de1ac14db6b44.

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_public_ip" "example" {
-  name                = "testpip1"
+  name                = "testpip"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
